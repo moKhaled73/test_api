cc653235ed3d1da3ad627cc1a043755cc026ba5b
moKhaled73 <mokhaled732003@gmail.com>
2024-08-16 08:55:43 +0300

    update cors

--- main.py
+++ main.py
@@ -10,9 +10,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Replace with specific origins in production
-    allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
 )
 
 
